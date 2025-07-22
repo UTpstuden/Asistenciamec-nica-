eed833faff706fcc0dcf8072496be61eee9d2874
Oscar Gutierrez <133546596+UTpstuden@users.noreply.github.com>
2025-07-22 09:48:57 -0500

    Delete Avance3/asistencia-mecanica directory

--- Avance3/asistencia-mecanica/src/main/java/com/gestion/asistencia_mecanica/config/Conexion.java
+++
@@ -1,1 +0,0 @@
-package com.gestion.asistencia_mecanica.config;
--- Avance3/asistencia-mecanica/src/main/java/com/gestion/asistencia_mecanica/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.gestion.asistencia_mecanica.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
